8545cbadef16eb6db033e1a17ce95b67da9e8181
vous <vous@ingainer.pro>
2025-05-16 19:49:12 +0100

    first one

---
+++ testnet_CSR.sh
@@ -0,0 +1,10 @@
+bp=$(/root/.local/share/solana/install/active_release/bin/solana block-production --url https://api.testnet.solana.com --output json-compact)
+totalBlocksProduced=$(echo $bp |jq -r '.total_slots')
+totalSlotsSkipped=$(echo $bp | jq -r '.total_slots_skipped')
+if [ -n "$totalBlocksProduced" ]; then
+	pctTotSkipped=$(echo "scale=2 ; 100 * $totalSlotsSkipped / $totalBlocksProduced" | bc)
+else 
+	pctTotSkipped=0
+fi
+
+echo $pctTotSkipped
---
+++ vote_credits_mainnet.py
@@ -0,0 +1,55 @@
+import subprocess
+import sys
+import requests
+import json
+
+RPC_URL = "https://api.mainnet-beta.solana.com"
+CLI = "/root/.local/share/solana/install/active_release/bin/solana"
+
+
+def get_validators(cli, url):
+    try:
+        cmd = subprocess.run([cli, 'validators', '--url', url, '--output', 'json-compact'],
+                             capture_output=True)
+        out = cmd.stdout
+    except FileNotFoundError as e:
+        sys.exit(str(e))
+    else:
+        return out
+
+
+def data_to_json(raw_data):
+    try:
+        obj = json.loads(raw_data)
+    except json.JSONDecodeError as e:
+        sys.exit(str(e))
+    else:
+        return obj
+
+
+def get_validators_from_json(json_object):
+    try:
+        validators = json_object["validators"]
+    except KeyError as e:
+        sys.exit(str(e))
+    else:
+        return validators
+
+
+if __name__ == '__main__':
+    grace = 3.00
+    epoch_credits = 0
+    v = get_validators(CLI, RPC_URL)
+    vobj = data_to_json(v)
+    cluster_validator_list = get_validators_from_json(vobj)
+    validators_count = len(cluster_validator_list)
+    val_cnt = 0
+    if validators_count > 0:
+        for validator in cluster_validator_list:
+            val_ep_creds = int(validator['epochCredits'])
+            if val_ep_creds > 0:
+                epoch_credits += val_ep_creds
+                val_cnt += 1
+            # epoch_credits += int(validator['epochCredits'])
+    min_epoch_credits = int(round((epoch_credits/val_cnt)*(1 - grace/100)))
+    print(min_epoch_credits)
